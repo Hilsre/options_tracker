4694700d73711e6ae8f93495538233c4c481f2ec
hilsr <153320902+Hilsre@users.noreply.github.com>
2025-05-29 14:55:22 +0200

    Database added

--- utils/db_helper.py
+++ utils/db_helper.py
@@ -3,7 +3,7 @@ import sqlite3
 
 def get_db():
     base_path = os.path.dirname(os.path.abspath(__file__))
-    db_path = os.path.join(base_path, "..", "db", "test.db")
+    db_path = os.path.join(base_path, "..", "db", "options_tracker.db")
     conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
     return conn
